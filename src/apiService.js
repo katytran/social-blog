87a80d87aba3c36d2fcfbbe3063ed729fe1f16e5
Do Thi Hai Van <annie@NGUYENs-MacBook-Pro.local>
2021-01-29 00:06:54 +0700

    Close #2: Create basic template

---
+++ src/apiService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const api = axios.create({
+  // baseURL: process.env.REACT_APP_BACKEND_API,
+  baseURL: "https://social-blog-cs.herokuapp.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+/**
+ * console.log all requests and responses
+ */
+api.interceptors.request.use(
+  (request) => {
+    console.log("Starting Request", request);
+    
+    return request;
+  },
+  function (error) {
+    console.log("REQUEST ERROR", error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("Response:", response);
+    return response;
+  },
+  function (error) {
+    error = error.response.data;
+    console.log("RESPONSE ERROR", error);
+    return Promise.reject({ message: error.errors.message});
+  }
+);
+
+export default api;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+const rootReducer = (state, action) => {
+  return state;
+};
+
+export default rootReducer;
+
