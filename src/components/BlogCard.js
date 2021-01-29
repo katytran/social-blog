0c71f2cb8e08eea66a881ee58f4d2fbd170a5009
Do Thi Hai Van <annie@NGUYENs-MacBook-Pro.local>
2021-01-30 00:46:14 +0700

    edit blog action,reducer, BlogCard and create HomePage(still have error)

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {Card} from "react-bootstrap";
+import Moment from 'react-moment';
 
-const BlogCard = ({ blog, handleClick }) => {
+const BlogCard = ({ blog, handleClickBlogCard }) => {
   return (
     <div>
-      <Card style={{ width: "18rem" }} onClick = {()=> handleClick(blog.id)}>
+      <Card style={{ width: "18rem" }} onClick = {()=> handleClickBlogCard(blog._id)}>
         <Card.Img variant="top" src={blog.images.length? blog.images[0] : "https://image.freepik.com/free-vector/travel-booklet-active-girls-cartoon-flat-ideas-varied-summer_81522-3183.jpg" } />
         <Card.Body>
           <Card.Title>{blog.title}</Card.Title>
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import BlogCard from "../components/BlogCard";
+import blogActions from "../redux/actions/blog.actions";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [pageNum, setPageNum] = useState(1);
+  const [field_name, setField_name] = useState("");
+  const blogs = useSelector((state) => state.blog.blogPosts);
+  const loading = useSelector((state) => state.blog.loading);
+  const limit = 10;
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch(blogActions.getPosts(pageNum, limit, field_name), [
+      dispatch,
+      pageNum,
+      limit,
+      field_name,
+    ]);
+  });
+  const handleClickBlogCard = (id) => {
+    history.push(`/blogs/${id}`)
+  }
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {/* {blogs.map((blog) => (
+            <BlogCard blog={blog} handleClickBlogCard={handleClickBlogCard} />
+          ))} */}
+          Homepage
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
