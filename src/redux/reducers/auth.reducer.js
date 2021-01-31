588835dd6be9c40aa7c122a8e628aadafdba89ae
Do Thi Hai Van <annie@NGUYENs-MacBook-Pro.local>
2021-01-31 23:45:27 +0700

    add profilePage(still have bugs)

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -10,12 +10,28 @@ const initialState = {
 const authReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
+    //login
     case types.LOGIN_REQUEST:
       return { ...state, loading: true };
     case types.LOGIN_SUCCESS:
       return { ...state, isAuthenticated: true, loading: false };
     case types.LOGIN_FAILURE:
       return { ...state, error: payload, loading: false };
+    //get current user
+    case types.GET_USER_REQUEST:
+      return {...state,loading: true};
+    case types.GET_USER_SUCCESS:
+      return {...state, isAuthenticated: true, loading: false, user: payload};
+    case types.GET_USER_FAILURE:
+      return {...state, loading: false};
+    //update profile
+    case types.UPDATE_PROFILE_REQUEST:
+      return {...state,loading: true};
+    case types.UPDATE_PROFILE_SUCCESS:
+      return {...state, user : {...state.user,payload}, loading: false};
+    case types.UPDATE_PROFILE_FAILURE:
+      return {...state, loading: false}
+    //logout
     case types.LOGOUT:
       return {
         ...state,
